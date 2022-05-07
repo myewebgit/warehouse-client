7cf3e4f3f69c5a39482e4e5e9dbcbccf88430314
myewebgit <mnislamctg7@gmail.com>
2022-05-07 23:24:05 +0600

    creating myItems button

--- src/Pages/ManageService/ManageService.js
+++ src/Pages/ManageService/ManageService.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Navigate, useNavigate } from 'react-router-dom';
 import useServices from '../../hooks/useServices';
+import  './ManageServices.css';
 
 
 const ManageServices = () => {
     const [services,setServices] = useServices();
+
+
+   
+    
+    
+const navigate = useNavigate();
+
+
+
     const handleDelete = id =>{
         const proceed = window.confirm('Are you sure?');
         if (proceed){
@@ -24,12 +35,17 @@ const ManageServices = () => {
     return(
         <div className='w-50 mx-auto'>
             <h5>Manage Services</h5>
+            <div className='order-cart'>
             {
-                services.map(service => <div key={service._id}>
-                    <h4>{service.name}<Button onClick={()=> handleDelete(service._id)}>Delete</Button></h4>
+                services.map(service => <div key={service._id}><img src={service.img} alt=""/>
+                    <h6>{service.name}<br></br><Button
+                     onClick={()=> handleDelete(service._id)}>Delete</Button>
+                     <Button onClick={(_id)=>navigate('/myitems')} > Order
+                        </Button></h6>
                     
                 </div>)
             }
+            </div>
         </div>
     )
 }
--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 const MyItems =()=>{
+
+    
     return (
         <div>
 		<h1> All are……………MyItems</h1>
+        
 
         </div>
     );
