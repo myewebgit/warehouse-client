1e2c45c959a204505644730ee1fb8f1b940cb12d
myewebgit <mnislamctg7@gmail.com>
2022-05-08 13:12:16 +0600

    Uploading top section images

--- src/Pages/TopSection/TopSection.js
+++ src/Pages/TopSection/TopSection.js
@@ -12,30 +12,35 @@ const TopSection =()=>{
 		<h1 className="text-center fst-italic text-primary"> New Arrivle!!!!<br></br> Book of this Month.</h1>
         <div class="card-group ">
   <div class="card">
-    <img src="https://i.ibb.co/CPVfQRM/glasses-1052010-340.jpg" height='300px' class="card-img-top" alt="..."/>
+    <img src="https://i.ibb.co/jMCb8Ph/MV5-BYzc2-MWU1-Nm-Mt-NTRl-OC00-ZWVk-LWE0-MDgt-Njc5-Mz-Ax-OGM4-ZWU1-Xk-Ey-Xk-Fqc-Gde-QXVy-NTE4-Mz-Ey.jpg" height='300px' class="card-img-top" alt="..."/>
     <div class="card-body">
-      <h5 class="card-title">The Pride of Jane Austen.</h5>
-      <p class="card-text">Jane Austen</p>
+      <h5 class="card-title">Nineteen Eighty-Four
+</h5>
+      <p class="card-text">Novel by George Orwell</p>
     </div>
     <div class="card-footer">
     <Button className="w-100" onClick={()=>navigate('/services')} >Detail</Button>
     </div>
   </div>
   <div class="card">
-    <img src="https://i.ibb.co/CPVfQRM/glasses-1052010-340.jpg" height='300px' class="card-img-top" alt="..."/>
+    <img src="https://i.ibb.co/M60P69W/19351043-SX0-SY0.jpg" height='300px' class="card-img-top" alt="..."/>
     <div class="card-body">
-      <h5 class="card-title">A Game of Thrones</h5>
-      <p class="card-text">George R. R. Martin</p>
+      <h5 class="card-title">Nimona
+
+</h5>
+      <p class="card-text">Novel by ND Stevenson</p>
     </div>
     <div class="card-footer">
     <Button className="w-100" onClick={()=>navigate('/services')} >Detail</Button>
     </div>
   </div>
   <div class="card">
-    <img src="https://i.ibb.co/CPVfQRM/glasses-1052010-340.jpg" height='300px' class="card-img-top" alt="..."/>
+    <img src="https://i.ibb.co/0mb1D3C/16160621.jpg" height='300px' class="card-img-top" alt="..."/>
     <div class="card-body">
-      <h5 class="card-title">Outlander</h5>
-      <p class="card-text">Author: Diana Gabaldon</p>
+      <h5 class="card-title">Sherlock Holmes 
+
+</h5>
+      <p class="card-text">Sir Arthur Conan Doyle</p>
     </div>
     <div class="card-footer">
     <Button className="w-100" onClick={()=>navigate('/services')} >Detail</Button>
@@ -45,30 +50,36 @@ const TopSection =()=>{
 </div>
 <div class="card-group">
   <div class="card">
-    <img src="https://i.ibb.co/CPVfQRM/glasses-1052010-340.jpg"  height='300px' class="card-img-top" alt="..."/>
+    <img src="https://i.ibb.co/D5g9WtS/71j-Jc-PTGd3-L.jpg"  height='300px' class="card-img-top" alt="..."/>
     <div class="card-body">
-      <h5 class="card-title">The Count of Monte Cristo</h5>
-      <p class="card-text">Author: Alexender Dumas</p>
+      <h5 class="card-title">The Name of the Wind
+
+</h5>
+      <p class="card-text">Novel by Patrick Rothfuss</p>
     </div>
     <div class="card-footer">
     <Button className="w-100" onClick={()=>navigate('/services')} >Detail</Button>
     </div>
   </div>
   <div class="card">
-    <img src="https://i.ibb.co/CPVfQRM/glasses-1052010-340.jpg" height='300px' class="card-img-top" alt="..."/>
+    <img src="https://i.ibb.co/VCzcqs5/24359968-SY475.jpg" height='300px' class="card-img-top" alt="..."/>
     <div class="card-body">
-      <h5 class="card-title">Maus</h5>
-      <p class="card-text">Author: Art Spigelman</p>
+      <h5 class="card-title">The Accursed Kings
+
+</h5>
+      <p class="card-text">Novel series by  Maurice Druon</p>
     </div>
     <div class="card-footer">
     <Button className="w-100" onClick={()=>navigate('/services')} >Detail</Button>
     </div>
   </div>
   <div class="card">
-    <img src="https://i.ibb.co/CPVfQRM/glasses-1052010-340.jpg" height='300px' class="card-img-top" alt="..."/>
+    <img src="https://i.ibb.co/wrWq9qH/download.jpg" height='300px' class="card-img-top" alt="..."/>
     <div class="card-body">
-      <h5 class="card-title">The Sweetness of Pie</h5>
-      <p class="card-text">Author: Alen Bardley</p>
+      <h5 class="card-title">Around the World in Eighty Days
+
+</h5>
+      <p class="card-text">Novel by Jules Verne and Peter Holeinone</p>
     </div>
     <div class="card-footer">
       <Button className="w-100" onClick={()=>navigate('/services')} >Detail</Button>
